88db527270ef5a123ec333fd117aaa5dfc04918d
Мишланов <mishlan0v@mail.ru>
2024-11-11 10:50:29 +0500

    Добавлено добавление и вывод всех заявок & чутка переписан класс order & добавлен test.http

--- be.cs
+++ be.cs
@@ -1,19 +1,27 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+List<order> bd = new List<order>()
+{
+    new order(1, DateTime.Now, "Ноутбук", "Проблема с экраном", "Экран треснул", "Иван Иванов", OrderStatus.в_ожидании)
+};
+
+app.MapGet("/", () => bd);
+
+app.MapPost("/", (order newOrder) =>
+{
+    newOrder.Number = bd.Any() ? bd.Max(o => o.Number) + 1 : 1;
+    newOrder.DateAdd = DateTime.Now;
+    newOrder.Status = OrderStatus.в_ожидании;
+
+    bd.Add(newOrder);
+
+    return Results.Created($"/{newOrder.Number}", newOrder);
+});
+
 
 app.Run();
-
-
-
-
-
-
-
-
-
-
 enum OrderStatus
 {
     в_ожидании = 1,
@@ -22,37 +30,25 @@ enum OrderStatus
     не_выполнено = 4,
 }
 
-
 class order
 {
-    int number;
-    DateTime DateAdd;
-    string device;
-    string problemType;
-    string problemDesc;
-    string client;
-    OrderStatus status = OrderStatus.в_ожидании;
-    string? master = "Не назначен";
+    public int Number { get; set; }
+    public DateTime DateAdd { get; set; }
+    public string Device { get; set; }
+    public string ProblemType { get; set; }
+    public string ProblemDesc { get; set; }
+    public string Client { get; set; }
+    public OrderStatus Status { get; set; } = OrderStatus.в_ожидании;
+    public string? Master { get; set; } = "Не назначен";
 
-    public order(int number, DateTime dateAdd, string device, string problemType, string problemDesc, string client, OrderStatus status, string? master)
+    public order(int number, DateTime dateAdd, string device, string problemType, string problemDesc, string client, OrderStatus status)
     {
-        this.number = number;
+        Number = number;
         DateAdd = dateAdd;
-        this.device = device;
-        this.problemType = problemType;
-        this.problemDesc = problemDesc;
-        this.client = client;
-        this.status = status;
-        this.master = master;
+        Device = device;
+        ProblemType = problemType;
+        ProblemDesc = problemDesc;
+        Client = client;
+        Status = status;
     }
-
-    public int Number { get => number; set => number = value; }
-    public DateTime DateAdd1 { get => DateAdd; set => DateAdd = value; }
-    public string Device { get => device; set => device = value; }
-    public string ProblemType { get => problemType; set => problemType = value; }
-    public string ProblemDesc { get => problemDesc; set => problemDesc = value; }
-    public string Client { get => client; set => client = value; }
-    public string? Master { get => master; set => master = value; }
-    internal OrderStatus Status { get => status; set => status = value; }
 }
-
